b14893cad0544348e2b49e1b92beaee60fc0110f
Rachel Gardner <rachelgardner9@gmail.com>
2018-02-03 20:34:39 -0800

    Fixed setup script (and added command to install curl

--- emacs/setup.bash
+++ emacs/setup.bash
@@ -2,8 +2,9 @@
 
 # Download emacs, then emacs prelude
 sudo apt install emacs
+sudo apt install curl
 curl -L https://git.io/epre | sh
 
 # Link customizations
 cd ~/.emacs.d/personal/preload
-ln -s ~/misc-scripts/config.el config.el
+ln -s ~/misc-scripts/emacs/config.el config.el
