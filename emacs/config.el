47282410e37f9a4fc57afe0e6734c28e8588e538
Rachel Gardner <rachelgardner9@gmail.com>
2020-06-15 12:09:07 -0700

    Add auto-save functionality

--- emacs/config.el
+++ emacs/config.el
@@ -52,6 +52,23 @@
 (global-set-key (kbd "C-x <") 'outdent-rigidly-4)
 (global-set-key (kbd "C-x >") 'indent-rigidly-4)
 
+; Taken from https://github.com/bbatsov/prelude/commit/f66a56a72edd38518a96cbba88822957275d108c
+(defcustom prelude-auto-save t
+  "Non-nil values enable Prelude's auto save."
+  :type 'boolean
+  :group 'prelude)
+
+(defun prelude-auto-save-command ()
+  (when (and prelude-auto-save
+             buffer-file-name
+             (buffer-modified-p (current-buffer)))
+    (save-buffer)))
+
+(defadvice switch-to-buffer (before save-buffer-now activate)
+  (prelude-auto-save-command))
+(defadvice other-window (before other-window-now activate)
+  (prelude-auto-save-command))
+
 (setq-default fill-column 80)
 (require 'comint)
 (setq comint-password-prompt-regexp
