d0c2927ac268ebea576e469b43abd8e81711710d
Rachel Gardner <rgardner@aurora.tech>
2021-10-02 19:10:05 -0700

    Add autocomplete and clipetty

--- emacs/config/autocomplete.el
+++ emacs/config/autocomplete.el
@@ -14,7 +14,15 @@
 (global-set-key (kbd "C-s") #'swiper)
 (global-set-key (kbd "M-x") #'counsel-M-x)
 (global-set-key (kbd "C-x C-f") #'counsel-find-file)
+(global-set-key (kbd "M-y") #'counsel-yank-pop)
 
 ;; Optionally, you can replace these default functions with Counsel version, too
 ;;(global-set-key (kbd "C-h f") 'counsel-describe-function)
 ;;(global-set-key (kbd "C-h v") 'counsel-describe-variable)
+(global-set-key (kbd "C-x C-f") #'counsel-find-file)
+
+(use-package yasnippet)
+(setq yas-snippet-dirs '("~/.emacs.d/config/snippets"))
+(yas/load-directory "~/.emacs.d/config/snippets")
+(setq yas/indent-line nil)
+(yas-global-mode 1)
--- emacs/config/ssh.el
+++ emacs/config/ssh.el
@@ -1,2 +1,4 @@
 ;; Sync clipboards over SSH (note: requires Kitty terminal emulator)
 (use-package clipetty)
+(global-clipetty-mode)
+
